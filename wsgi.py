7c23ffc737466ece10220ca5f51d708756e298cf
Javier <jav.dl.ponce@gmail.com>
2020-10-30 16:37:39 -0300

    implementando archivos django

---
+++ wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for El_Closet_Infantil project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'El_Closet_Infantil.settings')
+
+application = get_wsgi_application()
